884a128bab6db73fbb30b9e1970bfd7f67b30c96
Miëtek Bak <mietek@bak.io>
2019-03-26 12:19:45 +0000

    Add QC sources (v1.0)

---
+++ src/qc/Bib.v
@@ -0,0 +1,13 @@
+(** * Bib: Bibliography *)
+
+(**
+
+[Wadler and Blott 1989] Philip Wadler, Stephen Blott, _How to Make
+ad-hoc Polymorphism Less ad-hoc_. POPL 1989.
+http://dl.acm.org/citation.cfm?doid=75277.75283
+
+*)
+
+(* $Date: 2017-05-24 13:40:13 -0400 (Wed, 24 May 2017) $ *)
+
+(* Tue Oct 9 11:47:31 EDT 2018 *)
