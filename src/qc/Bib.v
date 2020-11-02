af98461414bb18c5b93bbdf5cb8b2196cb6b0e22
Yak! <yak1ex@users.noreply.github.com>
2020-11-02 17:12:54 +0900

    Add QC sources (v1.1).

---
+++ src/qc/Bib.v
@@ -0,0 +1,12 @@
+(** * Bib: Bibliography *)
+
+(**
+
+[Wadler and Blott 1989] Philip Wadler, Stephen Blott, _How to Make
+ad-hoc Polymorphism Less ad-hoc_. POPL 1989.
+https://dl.acm.org/citation.cfm?doid=75277.75283
+
+*)
+
+
+(* 2020-10-14 10:23 *)
---
+++ src/qc/Preface.v
@@ -0,0 +1,46 @@
+(** * Preface *)
+
+(** This volume of _Software Foundations_ introduces QuickChick, a
+    property-based random testing tool for Coq. *)
+
+(* ################################################################# *)
+(** * Setup *)
+
+(** For working with this material, you will need to install
+    QuickChick.  Instructions can be found here:
+
+       https://github.com/QuickChick/QuickChick
+
+    The [QuickChickInterface] chapter serves as a reference
+    manual for QuickChick.  The rest of the book explains its features
+    more gently. *)
+
+(* ################################################################# *)
+(** * Practicalities *)
+(* ================================================================= *)
+(** ** Recommended Citation Format *)
+
+(** If you want to refer to this volume in your own writing, please
+    do so as follows:
+
+   @book            {$FIRSTAUTHOR:SF$VOLUMENUMBER,
+   author       =   {$AUTHORS},
+   title        =   "$VOLUMENAME",
+   series       =   "Software Foundations",
+   volume       =   "$VOLUMENUMBER",
+   year         =   "$VOLUMEYEAR",
+   publisher    =   "Electronic textbook",
+   note         =   {Version $VERSION, \URLhttp://softwarefoundations.cis.upenn.edu },
+   }
+*)
+
+(* ################################################################# *)
+(** * Thanks *)
+
+(** Development of the _Software Foundations_ series has been
+    supported, in part, by the National Science Foundation under the
+    NSF Expeditions grant 1521523, _The Science of Deep
+    Specification_.  Work on this volume was also supported by NSF
+    grant 1421243, _Random Testing for Language Design_. *)
+
+(* 2020-10-14 10:23 *)
